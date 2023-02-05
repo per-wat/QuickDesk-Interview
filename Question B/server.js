c80fc66a6311606871ac28d30daab325d2465647
per-wat <ifwat.shamsuddin@gmail.com>
2023-02-06 04:36:55 +0800

    Changed to expressMiddleware

--- Question B/server.js
+++ Question B/server.js
@@ -1,20 +1,70 @@
 const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const { createServer } = require('http');
+const express = require('express');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { WebSocketServer } = require('ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { sequelize } = require("./models")
 
 const resolvers = require('./graphql/resolvers.js');
 const typeDefs = require('./graphql/typeDefs.js');
 
+// Create the schema, which will be used separately by ApolloServer and
+// the WebSocket server.
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Create an Express app and HTTP server; we will attach both the WebSocket
+// server and the ApolloServer to this HTTP server.
+const app = express();
+const httpServer = createServer(app);
+
+// Create our WebSocket server using the HTTP server we just set up.
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
+});
+// Save the returned server's info so we can shutdown this server later
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Set up ApolloServer.
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
 });
 
-startStandaloneServer(server).then(({url}) => {
-  console.log(`🚀 Server ready at ${url}`);
-  
+startServer = async () => {
+  await server.start()
+  .then(() => {
+    app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server));
+  })
+}
+startServer();
+
+const PORT = 4000;
+// Now that our HTTP server is fully set up, we can listen to it.
+httpServer.listen(PORT, () => {
+  console.log(`Server is now running on http://localhost:${PORT}/graphql`);
+
   sequelize.authenticate()
     .then(() => console.log("Db connected!"))
     .catch((err) => console.log(err))
-})
+});
