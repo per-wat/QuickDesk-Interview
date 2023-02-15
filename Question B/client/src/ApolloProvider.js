91191bb5e69453295cc5ca49c688fa99a2311483
per-wat <ifwat.shamsuddin@gmail.com>
2023-02-15 16:16:14 +0800

    Prep again for deployment

--- Question B/client/src/ApolloProvider.js
+++ Question B/client/src/ApolloProvider.js
@@ -9,13 +9,13 @@ import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/'
+  uri: '/graphql/'
 });
 
 const host = window.location.host
 
 const wsLink = new GraphQLWsLink(createClient({
-  url: `ws://localhost:4000/`,
+  url: `ws://${host}/graphql/`,
 }));
 
 // The split function takes three parameters:
